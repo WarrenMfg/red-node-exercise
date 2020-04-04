e1d9dd2acaf9c415cb0c7331b00b2a4f5d0056ef
WarrenMfg <warrenmfg@icloud.com>
2020-04-04 18:10:20 -0400

    improved all server tests

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,13 +1,29 @@
 import axios from 'axios';
 import faker from 'faker';
 
+const GET = () => {
+  return axios.get('http://localhost:50000/api/data/many/rank/asc')
+    .then(res => res.data)
+    .catch(err => console.log(err));
+};
+
+const POST = (record) => {
+  return axios.post('http://localhost:50000/api/data/one/rank/asc', record)
+    .then(res => res.data)
+    .catch(err => console.log(err));
+};
+
+const DELETE = (record) => {
+  return axios.delete('http://localhost:50000/api/data/one/rank/asc', { data: record })
+    .then(res => res.data)
+    .catch(err => console.log(err));
+};
+
 
 describe('server receives and responds to requests', () => {
 
   test('it should receive and respond to a GET request', async () => {
-    const records = await axios.get('http://localhost:50000/api/data/many/rank/asc')
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    const records = await GET();
 
     // assumes database is seeded
     expect(records.length).toBeGreaterThan(0);
@@ -31,9 +47,7 @@ describe('server receives and responds to requests', () => {
 
   test('it should receive and respond to a valid POST request', async () => {
     // GET records first to compare
-    const beforePOST = await axios.get('http://localhost:50000/api/data/many/rank/asc')
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    const beforePOST = await GET();
 
     // make POST request
     const validRecord = {
@@ -41,9 +55,7 @@ describe('server receives and responds to requests', () => {
       name: `${faker.name.firstName()} ${faker.name.lastName()}`,
       age: faker.random.number(100),
     };
-    const afterPOST = await axios.post('http://localhost:50000/api/data/one/rank/asc', validRecord)
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    const afterPOST = await POST(validRecord);
 
     // ensure it was POSTED
     expect(beforePOST.length + 1).toBe(afterPOST.length);
@@ -54,26 +66,70 @@ describe('server receives and responds to requests', () => {
         return record;
       }
     })[0];
-
     const map = new Map( Object.entries(validRecord) );
     map.forEach((val, key) => {
       expect(newRecord[key]).toBe(val);
     });
 
     // delete newRecord
-    const afterDELETE = await axios.delete('http://localhost:50000/api/data/one/rank/asc', { data: newRecord })
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    const afterDELETE = await DELETE(newRecord);
     
     expect(afterDELETE.length).toBe(beforePOST.length);
   });
 
 
-  test('it should receive and respond to an invalid POST request', async () => {
+  test('it should update ranks after a valid POST request', async () => {
     // GET records first to compare
-    const beforePOST = await axios.get('http://localhost:50000/api/data/many/rank/asc')
+    const beforePOST = await GET();
+
+    // ensure consecutive ranks
+    beforePOST.forEach((record, i, arr) => {
+      expect(record.rank).toBe(i + 1);
+      // ensure points descend
+      if (i < arr.length - 1) {
+        expect(record.points).toBeGreaterThanOrEqual(arr[i + 1].points);
+      }
+    });
+
+    // make POST request
+    const validRecord = {
+      points: faker.random.number(999999),
+      name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      age: faker.random.number(100),
+    };
+    const afterPOST = await POST(validRecord);
+
+    // ensure consecutive ranks
+    afterPOST.forEach((record, i, arr) => {
+      expect(record.rank).toBe(i + 1);
+      // ensure points descend
+      if (i < arr.length - 1) {
+        expect(record.points).toBeGreaterThanOrEqual(arr[i + 1].points);
+      }
+    });
+  });
+
+
+  test('it should receive and respond to an invalid POST request', async () => {
+    // make invalid POST request
+    const invalidRecord = {
+      points: "A lot of points",
+      name: undefined,
+      age: faker.random.number(100) + 1000
+    };
+    // cannot use POST() function because err needs to be returned
+    const POSTattempt = await axios.post('http://localhost:50000/api/data/one/rank/asc', invalidRecord)
       .then(res => res.data)
-      .catch(err => console.log(err));
+      .catch(err => err);
+
+    // ensure response status === 400
+    expect(POSTattempt.response.status).toBe(400);
+  });
+
+
+  test('it should not update ranks after an invalid POST request', async () => {
+    // GET records first to compare
+    const beforePOST = await GET();
 
     // make invalid POST request
     const invalidRecord = {
@@ -81,40 +137,35 @@ describe('server receives and responds to requests', () => {
       name: undefined,
       age: faker.random.number(100) + 1000
     };
-    const afterPOST = await axios.post('http://localhost:50000/api/data/one/rank/asc', invalidRecord)
-      .then(res => res)
+    // cannot use POST() function because err needs to be returned instead of logged
+    await axios.post('http://localhost:50000/api/data/one/rank/asc', invalidRecord)
+      .then(res => res.data)
       .catch(err => err);
 
-    // ensure it was not POSTED
-    expect(afterPOST.response.status).toBe(400);
+    // compare before and after for equality
+    const afterPOST = await GET();
+    expect(beforePOST).toEqual(afterPOST);
   });
 
 
   test('it should receive and respond to a DELETE request', async () => {
     // GET records first to compare
-    const beforeDELETE = await axios.get('http://localhost:50000/api/data/many/rank/asc')
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    const beforeDELETE = await GET();
     
     const deletedRecord = beforeDELETE[0];
 
-    const afterDELETE = await axios.delete('http://localhost:50000/api/data/one/rank/asc', { data: deletedRecord })
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    // DELETE records--returns updated array of all records
+    const afterDELETE = await DELETE(deletedRecord);
 
     // compare to ensure deletedRecord was deleted
     expect(beforeDELETE.length - 1).toBe(afterDELETE.length);
     expect(afterDELETE).not.toContain(deletedRecord);
-
-    // NOTE: test does not make follow-up POST request; ensure database is fully seeded
   });
 
 
   test('it should update ranks after a DELETE request', async () => {
     // GET records first to compare
-    const beforeDELETE = await axios.get('http://localhost:50000/api/data/many/rank/asc')
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    const beforeDELETE = await GET();
     
     // ensure consecutive ranks
     beforeDELETE.forEach((record, i, arr) => {
@@ -127,9 +178,8 @@ describe('server receives and responds to requests', () => {
     
     const deletedRecord = beforeDELETE[0];
 
-    const afterDELETE = await axios.delete('http://localhost:50000/api/data/one/rank/asc', { data: deletedRecord })
-      .then(res => res.data)
-      .catch(err => console.log(err));
+    // DELETE records--returns updated array of all records
+    const afterDELETE = await DELETE(deletedRecord);
 
     // ensure consecutive ranks
     afterDELETE.forEach((record, i, arr) => {
@@ -142,13 +192,9 @@ describe('server receives and responds to requests', () => {
   });
 
 
-  // test('it should update ranks after a valid POST request', async () => {
-
-  // });
+  
 
 
-  // test('it should not update ranks after an invalid POST request', async () => {
-    
-  // });
+  
 });
 
